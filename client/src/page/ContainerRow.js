060a3603dd025f2461434551f59606909154b89b
Dennis Pidun <dennis.pidun@hotmail.de>
2023-03-12 23:07:13 +0100

    wip

--- client/src/page/ContainerRow.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import styles from './ContainerRow.module.css';
-const ContainerRow = ({ centered, children }) => {
-    return _jsx("div", { className: centered && styles.center, children: children });
-};
-export default ContainerRow;
